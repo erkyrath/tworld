1f778c2934ff2ff60e27426761c8fb9c756cb362
Andrew Plotkin <erkyrath@eblong.com>
2013-05-03 01:32:39 -0400

    Not actually making progress with the ioloop.

--- lib/two/app.py
+++ lib/two/app.py
@@ -15,7 +15,9 @@ class Tworld(object):
     def listen(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         self.listensock = sock
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+            (sock.getsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR) | 1))
         sock.setblocking(0)
         sock.bind( ('localhost', self.opts.tworld_port) )
         sock.listen(32)
@@ -28,12 +30,27 @@ class Tworld(object):
         self.log.info('Listening on port %d', self.opts.tworld_port)
 
     def listen_ready(self, fd, events):
+        self.log.info('### read_ready %d', events)
         while True:
             try:
                 (sock, addr) = self.listensock.accept()
+                (host, port) = addr
             except socket.error as ex:
+                self.log.info('### accept error %s', ex)
                 if ex.args[0] not in (errno.EWOULDBLOCK, errno.EAGAIN):
                     raise
+                self.log.info('### returning')
                 return
+            
             sock.setblocking(0)
-            self.log.info('Accepted connection from %s', addr)
+            tornado.ioloop.IOLoop.instance().add_handler(
+                sock.fileno(),
+                self.read_ready,
+                tornado.ioloop.IOLoop.READ | tornado.ioloop.IOLoop.ERROR)
+            self.log.info('Accepted connection from %s', host)
+            sock = None
+
+    def read_ready(self, fd, events):
+        self.log.info('### read_ready %d', events)
+
+    
