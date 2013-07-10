6d0cf3303bc2e9f73ba1c2ec3cefdece0086647e
Andrew Plotkin <erkyrath@eblong.com>
2013-07-10 18:14:58 -0400

    Call it /eval instead of /exec

--- lib/two/commands.py
+++ lib/two/commands.py
@@ -852,23 +852,23 @@ def define_commands():
 
         yield ctx.perform_move(location['_id'], 'Your location changes.', False, None, False, None, False)
         
-    @command('meta_exec', restrict='creator', doeswrite=True)
-    def cmd_meta_exec(app, task, cmd, conn):
+    @command('meta_eval', restrict='creator', doeswrite=True)
+    def cmd_meta_eval(app, task, cmd, conn):
         if len(cmd.args) == 0:
-            raise MessageException('Usage: /exec code')
+            raise MessageException('Usage: /eval code')
         code = ' '.join(cmd.args)
         loctx = yield task.get_loctx(conn.uid)
         ctx = two.evalctx.EvalPropContext(task, loctx=loctx, level=LEVEL_EXECUTE)
         try:
             newval = yield ctx.eval(code, evaltype=EVALTYPE_CODE)
             if newval is None:
-                conn.write({'cmd':'event', 'text':'Exec done.'})
+                conn.write({'cmd':'event', 'text':'Eval done.'})
             else:
-                conn.write({'cmd':'event', 'text':'Exec done: %s' % (repr(newval),)})
+                conn.write({'cmd':'event', 'text':'Eval done: %s' % (repr(newval),)})
         except Exception as ex:
-            task.log.warning('Exec failed: %s', ex, exc_info=app.debugstacktraces)
+            task.log.warning('Eval failed: %s', ex, exc_info=app.debugstacktraces)
             exmsg = '%s: %s' % (ex.__class__.__name__, ex,)
-            conn.write({'cmd':'event', 'text':'Exec raised exception: %s' % (exmsg,)})
+            conn.write({'cmd':'event', 'text':'Eval raised exception: %s' % (exmsg,)})
         
     @command('meta_holler', restrict='admin')
     def cmd_meta_holler(app, task, cmd, conn):
