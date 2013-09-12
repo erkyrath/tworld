2a13a74869220c8e43d9e288f53e1c0d8a99ef0d
Andrew Plotkin <erkyrath@eblong.com>
2013-09-12 13:35:25 -0400

    Make the pw-change form work without javascript.

--- static/js/account.js
+++ static/js/account.js
@@ -6,6 +6,12 @@ function evhan_change_pw(ev) {
 
 /* The page-ready handler. Like onload(), but better, I'm told. */
 $(document).ready(function() {
+    /* Hide the pw-change form. (We do this in JS so that it's available
+       by default when JS is turned off.) */
+    if (!show_pwchange_initially) {
+        $('#change_pw_form').hide();
+    }
+
     $('#change_pw_link').on('click', evhan_change_pw);
 });
 
