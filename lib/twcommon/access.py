1de2b4583ba88de04d29ee1861621f035bb6bfa1
Andrew Plotkin <erkyrath@eblong.com>
2013-10-27 12:45:38 -0400

    Comments, and dict comprehensions instead of conversion

--- lib/twcommon/access.py
+++ lib/twcommon/access.py
@@ -13,16 +13,24 @@ defs = [
     ('FOUNDER', ACC_FOUNDER),
     ]
 
-map    = dict([ (key,val) for (key,val) in defs ])
-revmap = dict([ (val,key) for (key,val) in defs ])
+map    = { key:val for (key,val) in defs }
+revmap = { val:key for (key,val) in defs }
 
 def level_named(val):
+    """Return the access level constant (int) with a given name. The name
+    is case-insensitive. If the name is not found, raises KeyError.
+    """
     val = val.upper()
     return map[val]
 
 def name_for_level(val):
+    """Return the name (upper-case string) for a given access level.
+    """
     return revmap[val]
 
 def level_name_list():
+    """Return the list of access level names as a string:
+        '"banned", "visitor", "member", "owner", "founder"'
+    """
     ls = [ '"'+val.lower()+'"' for (val, dummy) in defs ]
     return ', '.join(ls)
